ef2782ad453c1e4d4c6f967585888403a7063029
Delores Diei <delores.diei@andela.com>
2017-12-07 18:22:59 +0100

    setup environments

--- django_cms/settings/base.py
+++ django_cms/settings/base.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cms_api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
